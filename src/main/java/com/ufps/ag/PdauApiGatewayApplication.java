757334c984eb4d043ae73e335c95cae4f1eb9d33
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-11-04 11:57:35 -0500

    first commit

---
+++ src/main/java/com/ufps/ag/PdauApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ufps.ag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdauApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdauApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufps/ag/PdauApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufps.ag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdauApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
